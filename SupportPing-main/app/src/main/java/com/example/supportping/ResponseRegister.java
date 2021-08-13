82e3478a4a739532877ff1ba9537b997b0a88b57
aahspringaa4 <aahspringaa4@naver.com>
2021-08-13 22:01:09 +0900

    [MODIFY] 불필요한 코드 정리

--- SupportPing-main/app/src/main/java/com/example/supportping/ResponseRegister.java
+++ SupportPing-main/app/src/main/java/com/example/supportping/ResponseRegister.java
@@ -2,11 +2,9 @@ package com.example.supportping;
 
 public class ResponseRegister {
     String name;
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
