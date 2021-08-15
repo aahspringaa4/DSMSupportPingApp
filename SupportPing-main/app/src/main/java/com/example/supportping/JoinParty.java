59ec06c8719e2e032ab210ed88d0043310615930
aahspringaa4 <aahspringaa4@naver.com>
2021-08-15 17:18:48 +0900

    [MODIFY]  파일 정리

--- SupportPing-main/app/src/main/java/com/example/supportping/JoinParty.java
+++ SupportPing-main/app/src/main/java/com/example/supportping/JoinParty.java
@@ -11,6 +11,5 @@ public class JoinParty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_party);
-
     }
 }
