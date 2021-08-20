c85ed11de13198aba75d2fdaa5e5b49bb038b5cc
aahspringaa4 <aahspringaa4@naver.com>
2021-08-20 14:30:40 +0900

    [MODIFY] 파일 이름 수정 (최종본)

--- SupportPing Final/app/src/main/java/com/example/supportping/RetrofitClient.java
+++ SupportPing Final/app/src/main/java/com/example/supportping/RetrofitClient.java
@@ -12,8 +12,6 @@ public class RetrofitClient {
     //사용하고 있는 서버 BASE 주소
     private static String baseUrl = "http://18.219.215.9:8081/";
 
-
-
     private RetrofitClient() {
         //로그를 보기 위한 Interceptor
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
@@ -38,7 +36,6 @@ public class RetrofitClient {
         }
         return instance;
     }
-
     public static ServerAPI getRetrofitInterface() {
         return serverAPI;
     }
